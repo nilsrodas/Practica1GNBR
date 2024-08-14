e3ceddd4a315585d625e161b4fc6f5ada1b8bea7
Graham Nils Butter Rodas <ia.nilsrodas@ufg.edu.sv>
2024-08-13 19:59:57 -0600

    Creación de index.php con estructura html

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> GRAHAM NILS BUTTER RODAS</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+        <script src= "js/jquery-3.7.1.min.js"></script>
+        <script src= "js/boostrap.bundle.min.js"></script>
+    </head>
+    <body>
+    
+    </body>
+
+</html>
